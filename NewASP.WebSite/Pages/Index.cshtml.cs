5179eeca1157013012d4312d4b3ee31b092760a3
SubhiNajjar <SubhiNajjar@users.noreply.github.com>
2020-08-24 09:49:27 +0200

    Web Site
    ASP/API

---
+++ NewASP.WebSite/Pages/Index.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using NewASP.WebSite.Models;
+using NewASP.WebSite.Services;
+
+namespace NewASP.WebSite.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public JsonFileTrendingService TrendingService;
+
+        public IEnumerable<Trending> Trendings { get; private set; }
+
+        public IndexModel(
+            ILogger<IndexModel> logger,
+            JsonFileTrendingService trendingService)
+        {
+            _logger = logger;
+            TrendingService = trendingService;
+        }
+
+        public void OnGet()
+        {
+            Trendings = TrendingService.GetTrendings();
+
+        }
+    }
+}
---
+++ NewASP.WebSite/Services/JsonFileTrendingService.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using NewASP.WebSite.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace NewASP.WebSite.Services
+{
+    public class JsonFileTrendingService
+    {
+        public JsonFileTrendingService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+        public IWebHostEnvironment WebHostEnvironment { get; }
+
+        private string JsonFileName
+        {
+            get { return Path.Combine(WebHostEnvironment.WebRootPath, "data", "Trendings.json"); }
+        }
+
+        public IEnumerable<Trending> GetTrendings()
+        {
+            using (var jsonFileReader = File.OpenText(JsonFileName))
+            {
+                return JsonSerializer.Deserialize<Trending[]>(jsonFileReader.ReadToEnd(),
+                    new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    });
+            }
+        }
+    }
+}
